fdbcd373226e45ad2e330c20768c5bd3156b6563
zhangchunxiao <zhangchunxiao@didichuxing.com>
2017-03-28 16:06:54 +0800

    update dist

--- compiled/methods/is-date-filter.js
+++ compiled/methods/is-date-filter.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = function (column) {
-  return this.query.hasOwnProperty(column) && this.opts.dateColumns.indexOf(column) > -1;
+  return this.query && this.query.hasOwnProperty(column) && this.opts.dateColumns.indexOf(column) > -1;
 };
